51aa1d97c44b0edcf6230f513c7c71a5465481dc
ChongLeh <85861224+ChongLeh@users.noreply.github.com>
2021-08-28 21:41:21 +0800

    Revert to lecture mergesort
    also Placed both sorts into same for loop

--- sorting_algorithms/src/sorting_algorithms/HybridSort.java
+++ sorting_algorithms/src/sorting_algorithms/HybridSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class HybridSort {
-	public static int s = 6; // Threshold should be in logn(?)
+	public static int s = 10; // Threshold should be in logn(?)
 	public static int comp;
 
 	private static void hybridSort(int[] arr, int left, int right) {
@@ -31,58 +31,44 @@ public class HybridSort {
 		merge(arr,n,m);
 	}
 
-	//Merge with auxiliary storage
+	//Merge with no auxiliary storage
 	static void merge(int arr[], int n, int m) {
+			
+		int mid = (n+m)/2;
+		int i=n;//1st element of 1st half
+		int j=mid+1;//1st element of 2nd half
+		if(m-n<=0) return;
 		
-		if(arr==null) return;
-		
-		int mid = n+(m-n)/2;
-		
-		int h1 = mid - n + 1;
-        int h2 = m - mid;
- 
-        //Split array into 2 halves
-        int L[] = new int[h1];
-        int R[] = new int[h2];
- 
-        //Copy elements into each halves respectively
-        for (int i = 0; i < h1; ++i)
-            L[i] = arr[n + i];
-        for (int j = 0; j < h2; ++j)
-            R[j] = arr[mid + 1 + j];
- 
-        // Initial indexes of first and second halves
-        int i = 0, j = 0;
- 
-        // Initial index of merged list
-        int k = n;
-        while (i < h1 && j < h2) {
-        	comp++;
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
- 
-        // Copy remaining elements of L[] into merged list
-        while (i < h1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
- 
-        // Copy remaining elements of R[] into merged list
-        while (j < h2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-		
+		//while both halves are not empty
+		while(i<=mid && j<=m) {
+			comp++;
+			//compare 1st element of both halves
+			if(arr[i]<arr[j]) {
+				i++; //no shifting done as element in correct position
+			}
+			else if(arr[i]>arr[j]) {
+				int temp = arr[j]; //1st element of 2nd half into temp
+				//shift all elements towards 'j' position
+				for(int x=j;x>i;x--) {
+					arr[x] = arr[x-1];
+				}
+				arr[i] = temp;//1st element of 2nd half joins end of merged list
+				//Increment all position by one
+				j++;
+				i++;
+				mid++;
+			}
+			else { //if 1st elements of 2 halves are equal
+				if(i==mid && j==m) break; //if last element
+				for(int x=j;x>i;x--) { ////shift all elements towards 'j' position
+					arr[x] = arr[x-1];
+				}
+				arr[i+1] = arr[i]; //Copy element into position behind end of merged list
+				i+=2;
+				j++;
+				mid++;
+			}
+		}
 	}
 	
 	private static void insertionSort(int[] ar, int left, int right) {
@@ -94,6 +80,7 @@ public class HybridSort {
 				comp++;
 				swap(ar, j - 1, j);
 			}
+			comp++;
 		}
 	}
 
@@ -102,7 +89,6 @@ public class HybridSort {
 		ar[i] = ar[j];
 		ar[j] = tmp;
 	}
-	
 	
 
 	static void printArray(int arr[]) {
@@ -128,12 +114,15 @@ public class HybridSort {
 		int arrSize = 1000;
 		double avgTime =0;
 		long avgComp = 0;
+		double avgTime2 = 0;
+		long avgComp2 = 0;
 		
 		//Start of hybridsort
-		for(int x=0;x<1000;x++) {
+		for(int x=0;x<10000;x++) {
 			comp = 0;
 			int arr[] = new int[arrSize];
-			arr = randomArray(arrSize); // Generate random array
+			arr = randomArray(arrSize);// Generate random array
+			int arr2[] = arr.clone();
 			
 			
 			//System.out.println("Given Array");
@@ -145,16 +134,26 @@ public class HybridSort {
 			double stopTime = System.nanoTime();
 			avgTime+=(stopTime-startTime)/1000000;
 			avgComp+=comp;
+			//End of hybridsort
+			//Start of merge sort
+			comp = 0;
+			arr = randomArray(arrSize); // Generate random array
+			double startTime2 = System.nanoTime();
+			mergesort(arr2, 0, arr2.length-1);
+			double stopTime2 = System.nanoTime();
+			avgTime2+=(stopTime2-startTime2)/1000000;
+			avgComp2+=comp;
+			//End of merge sort
 		}
-		//End of hybridsort
 		
-		System.out.println("Average computational time of Hybrid Sort: " + avgTime/1000);
+		
+		System.out.println("Average computational time of Hybrid Sort: " + avgTime/1000 + "ms");
 		System.out.println("Average comparisons of Hybrid Sort: " + avgComp/1000);
-		
+		/*
 		//Start of mergesort
 		avgComp = 0;
-		avgTime =0;
-		for(int x=0;x<1000;x++) {
+		avgTime = 0;
+		for(int x=0;x<10000;x++) {
 			comp = 0;
 			int arr[] = new int[arrSize];
 			arr = randomArray(arrSize); // Generate random array
@@ -165,8 +164,9 @@ public class HybridSort {
 			avgComp+=comp;
 		}
 		//End of mergesort
-		System.out.println("\nAverage computational time of Merge Sort: "+avgTime/1000);
-		System.out.println("Average comparisons of Merge Sort: " + avgComp/1000);
+		 */
+		System.out.println("\nAverage computational time of Merge Sort: "+avgTime2/1000 + "ms");
+		System.out.println("Average comparisons of Merge Sort: " + avgComp2/1000);
 		
 	}
 
